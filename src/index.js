6c622838743416ea98bd2c996fdbb07e1d6f4df2
Fiquell <fiquellh@gmail.com>
2025-11-21 10:44:43 +0800

    rename urlRoutes to urlRouter for consistency

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import "dotenv/config";
 
 import express from "express";
 import { connectDB } from "./config/db.js";
-import urlRoutes from "./routes/url.routes.js";
+import urlRouter from "./routes/url.routes.js";
 import welcomeRouter from "./routes/welcome.routes.js";
 
 const app = express();
 
 app.use(express.json());
+
 app.use("/", welcomeRouter);
-app.use("/", urlRoutes);
+app.use("/", urlRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
